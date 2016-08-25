ef150afba1f8c13dbc63d27e4c324b0d4c990859
Thomas Lee <tleehoc@gmail.com>
2016-08-25 16:00:57 -0700

    Fix null on non IE user agents.

--- src/mutationobserver-polyfill.js
+++ src/mutationobserver-polyfill.js
@@ -7,6 +7,9 @@
    * Regex from https://github.com/faisalman/ua-parser-js
    */
   var parts = navigator.userAgent.match(/(trident).+rv[:\s]([\w\.]+).+like\sgecko/i)
+  if (!parts) {
+    return
+  }
   var browserName = parts[1]
   var browserVersion = parts[2]
   if (browserName == 'Trident' && browserVersion == '11.0') {
